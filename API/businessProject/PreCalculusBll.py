71d5d66d7c776d5d2858d5d8d644f2e407cbadc8
Oscar Andrés Russi Porras <oscarporras02@gmail.com>
2018-11-11 22:58:51 -0500

    third problem added for precalculus, exam 1

--- API/businessProject/PreCalculusBll.py
+++ API/businessProject/PreCalculusBll.py
@@ -58,13 +58,45 @@ def typeSeriesProblem():
             function=str(addNumber)+"+ln(x^("+str(opNumber)+"))"
             solution = "decreasing" if opNumber<0 else "increasing"
         question = "for de next function  "+function+", define the series type when x tends to infinity"
-        options =json.dumps({'a':'increasing', 'b':'decreasing', 'c':'alterning', 'c':'undefined'})
+        options =json.dumps({'a':'increasing', 'b':'decreasing', 'c':'alterning', 'd':'undefined'})
         jsonResponse = json.dumps({'question':question, 'solution':solution, 'options':options})
         return jsonResponse
     except Exception as er:
         return er
-
-exam1 =[seriesProblem, typeSeriesProblem]
+#for the function define the type of series
+def boundedSeriesProblem():
+    try:
+        a = random.randint(1,5)*(random.randint(0,1)*2-1)
+        b = random.randint(1,5)*(random.randint(0,1)*2-1)
+        c = random.randint(1,5)*(random.randint(0,1)*2-1)
+        op = random.randint(1,6)
+        function=""
+        solution=""
+        if(op==1):
+            function=str(a)+"*(x^"+str(abs(b*2))+")"
+            solution = "superior bounded" if a<0 else "inferior bounded"
+        elif(op==2):
+            function=str(a)+"*ln("+str(b)+"x)"
+            solution = "superior bounded" if a>0 else "inferior bounded"
+        elif(op==3):
+            function=str(abs(a))+"/((x+("+str(b)+"))^("+str(c)+"))"
+            solution = "not bounded" if c%2==0 else "inferior bounded"
+        elif(op==4):
+            function=str(abs(a))+"/(("+str(b)+")^(x+"+str(c)+"))"
+            solution = "not bounded" if b<0 else "inferior bounded"
+        elif(op==5):
+            function=str(abs(a))+"x/(("+str(abs(b))+")+("+str(c)+"x^2))"
+            solution = "not bounded" if c<0 else "bounded"
+        else:
+            function="cos(x)*(sin(x)^("+str(a)+"))"
+            solution = "not bounded" if a<0 else "bounded"
+        question = "for de next function  "+function+", define the series type"
+        options =json.dumps({'a':'inferior bounded', 'b':'superior bounded', 'c':'not bounded', 'd':'bounded'})
+        jsonResponse = json.dumps({'question':question, 'solution':solution, 'options':options})
+        return jsonResponse
+    except Exception as er:
+        return er
+exam1 =[seriesProblem, typeSeriesProblem, boundedSeriesProblem]
 exam2 =[]
 listMethods = [exam1, exam2]
 def generateExam(unit):
