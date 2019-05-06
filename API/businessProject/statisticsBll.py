b1571399aedb15b40bb1531345574374d85d78af
Oscar Andrés Russi Porras <oscarporras02@gmail.com>
2019-05-05 22:39:02 -0500

    added problem 12 in statistics, exam 1

--- API/businessProject/statisticsBll.py
+++ API/businessProject/statisticsBll.py
@@ -392,7 +392,30 @@ def statisticsConceptProblem():
     except Exception as er:
         return er
 
-exam1 = [uniformDistributionProblem, continuousVarianceProblem, uniformVarianceProblem, categoricalQuantitativeProblem, samplingProblem, plotBoxOutlierProblem, chebyshevProblem, linearRegressionProblem, coefficientDeterminationProblem, correlationRulesProblem, statisticsConceptProblem]
+def expectedVarianceProblem():
+    try:
+        m=random.randint(20,100)
+        p= random.randint(200,1000)
+        s2=random.randint(100,200)
+        sol1 = round(s2/m,4)
+        sol2 = round((s2/m)*((p-m)/(p-1)),4)
+        solution=r"a) "+str(sol1)+r", b) "+str(sol2)+r""
+        question=r'If you make a sample of size '+str(m)+r' from a population with \sigma^{2} = '+str(s2)+r' a) find the expected variance if population size is infinite b) expected variance if population size is '+str(p)+r': '
+        alternatives = coursesFunctionsBll.multipleOptions([sol1, sol2],5)
+        tempAlternatives =[]
+        for ta in range(5):
+            tempAlternatives.append(r"a) "+str(alternatives[ta][0])+r", b) "+str(alternatives[ta][1])+r"")
+        options =json.loads(json.dumps({'a':tempAlternatives[0],
+                                        'b':tempAlternatives[1], 
+                                        'c': tempAlternatives[2], 
+                                        'd': tempAlternatives[3], 
+                                        'e': tempAlternatives[4]}))
+        jsonResponse = json.dumps({"question":coursesFunctionsBll.replaceSpace(question), "solution":coursesFunctionsBll.replaceSpace(solution), "options":coursesFunctionsBll.replaceOptions(options)})
+        return jsonResponse
+    except Exception as er:
+        return er
+
+exam1 = [uniformDistributionProblem, continuousVarianceProblem, uniformVarianceProblem, categoricalQuantitativeProblem, samplingProblem, plotBoxOutlierProblem, chebyshevProblem, linearRegressionProblem, coefficientDeterminationProblem, correlationRulesProblem, statisticsConceptProblem, expectedVarianceProblem]
 exam2 = []
 listMethods = [exam1, exam2]
 def generateExam(unit):
